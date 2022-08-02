02ddc434d5d724b291cec7bb2084eb8011ebe701
Mario Vizcaino <mario.vizcainos@gmail.com>
2022-08-01 21:35:34 -0500

    MOD: refactor pagination method

--- utils/pagination.js
+++ utils/pagination.js
@@ -1,10 +1,12 @@
 export default async function (model, page = 1, limit = 10) {
-  const total_items = await model.find().count();
+  const offset = (page - 1) * limit + 1 - 1;
+  const result = await model.find().skip(offset).limit(limit);
 
+  const total_items = await model.find().count();
   const total_pages = total_items > 0 ? Math.ceil(total_items / limit) : 1;
   const prev = page > 1 ? page - 1 : null;
-  const next =  page < total_pages ? page + 1 : null;
-  return {
+  const next = page < total_pages ? page + 1 : null;
+  const pagination = {
     total_items,
     total_pages,
     limit,
@@ -12,4 +14,5 @@ export default async function (model, page = 1, limit = 10) {
     prev,
     next,
   };
+  return [result, pagination];
 }
