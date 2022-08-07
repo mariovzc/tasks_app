b54f5b7d841c177c1b422aa8704655dba4908c89
Mario Vizcaino <mario.vizcainos@gmail.com>
2022-08-06 23:18:34 -0500

    ADD: validation to all modules

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,18 @@
 import taskRouter from './tasks.js';
 import UserRouter from './users.js';
 import AuthRouter from './auth.js';
+import passport from 'passport';
 
 export default function routerApi(app) {
-  app.use('/tasks', taskRouter);
-  app.use('/users', UserRouter);
+  app.use(
+    '/tasks',
+    passport.authenticate('jwt', { session: false }),
+    taskRouter
+  );
+  app.use(
+    '/users',
+    passport.authenticate('jwt', { session: false }),
+    UserRouter
+  );
   app.use('/auth', AuthRouter);
 }
